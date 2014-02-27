bef8f1e118f566bfccf885a5aaf4a309f98492e8
Fawaz Fernand PARAISO <fifiant@gmail.com>
2014-02-27 09:19:07 +0100

    update

--- push_release.sh
+++ push_release.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Pushes the contents of this repository, called automatically from release scripts using this repository.
 
-mvn release:prepare -DdryRun=true
+# Go to script dir
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+# Add repository dir to git and push
+git add repository && git commit -m"New Release" && git push
